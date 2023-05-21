b33d88ab5d11fc3779095911b57af22a1eb59ce0
chinatsu <i.chinatsu0274@gmail.com>
2023-05-21 11:42:55 +0000

    devise修正

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,5 @@
 # Load the Rails application.
 require_relative "application"
-const webpack = require('webpack')
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    jquery: 'jquery/src/jquery',
-  })
-)
 
 # Initialize the Rails application.
 Rails.application.initialize!
